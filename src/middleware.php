ed248c1489c4e69047110215664179ba406674a3
Michiel Decoster <decoster.michiel@gmail.com>
2020-03-07 15:30:23 +0100

    Changed hardcoded to .env

--- src/middleware.php
+++ src/middleware.php
@@ -5,10 +5,14 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\App;
 
+$dotenv = DotEnv\Dotenv::createImmutable(_DIR_);
+$dotenv->load;
+$dotenv->required(['PHP_URL']);
+
 return function (App $app) {
     $app->add(function (Request $request, RequestHandler $handler) {
         $response = $handler->handle($request);
         return $response->withHeader('Content-Type', 'application/json')
-                      ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+                      ->withHeader('Access-Control-Allow-Origin', getenv('PHP_URL'));
     });
 };
