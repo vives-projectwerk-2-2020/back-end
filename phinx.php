ddcb13d88d8c1e9020206c78d12bf80dac75c229
git <arno.willems@student.vives.be>
2020-03-29 16:15:46 +0200

    adjusted .env usage

--- phinx.php
+++ phinx.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-// $dotenv->required(['MYSQL_DRIVER', 'MYSQL_HOST', 'MYSQL_DATABASE', 'MYSQL_USER', 'MYSQL_PASSWORD']);
+$dotenv->required(['MYSQL_DRIVER', 'MYSQL_HOST', 'MYSQL_DATABASE', 'MYSQL_USER', 'MYSQL_PASSWORD']);
 
 return [
   'paths' => [
@@ -15,11 +15,11 @@ return [
     'default_migration_table' => 'phinxlog',
     'default_database' => 'dev',
     'dev' => [
-      'adapter' => getenv('MYSQL_DRIVER'),
-      'host' => getenv('MYSQL_HOST'),
-      'name' => getenv('MYSQL_DATABASE'),
-      'user' => getenv('MYSQL_USER'),
-      'pass' => getenv('MYSQL_PASSWORD'),
+      'adapter' => $_ENV['MYSQL_DRIVER'],
+      'host' => $_ENV['MYSQL_HOST'],
+      'name' => $_ENV[('MYSQL_DATABASE'],
+      'user' => $_ENV['MYSQL_USER'],
+      'pass' => $_ENV['MYSQL_PASSWORD'],
       'port' => 3306
     ]
   ]
