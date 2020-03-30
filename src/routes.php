9f51c7b23cf95e08471608a22f48571ded190aac
botn365 <Krampus.sack.never@gmail.com>
2020-03-30 16:48:53 +0200

    fixet route

--- src/routes.php
+++ src/routes.php
@@ -27,7 +27,7 @@ return function (App $app) {
     {
         $client = new InfluxDB\Client(getenv('INFLUX_IP'), getenv('INFLUX_PORT'));
         $database = $client->selectDB('particulaInfluxDB');
-        $result = $database->query('select * from sensors');
+        $result = $database->query('select * from sensors GROUP BY sensor_id');
         $response->getBody()->write(json_encode($result->getPoints()));
         return $response;
     });
@@ -44,32 +44,23 @@ return function (App $app) {
         //put the time parameter in easyer to process way
         $period_range =  substr($period,-1);
         $period_time = (int)substr($period,0,-1);
-        if ($period_time == "y")
+        if ($period_range == "y")
         { 
             $period_time *= 365;
+            $period_range = "d";
+
         }
+        
         //untested
         $new_date = $period_time . $period_range;
-        $result = $database->query("select pm10,pm25,temperature,humidity FROM sensors WHERE sensor_id = $id AND time > now() - $new_date");
-        $response->getBody()->write(json_encode($result->getPoints()));
-        return $response;
-    });
-    $app->get('/sensors', function($request, $response)
-    {
-        $client = new InfluxDB\Client(getenv('INFLUX_IP'), getenv('INFLUX_PORT'));
-        $database = $client->selectDB('particulaInfluxDB');
-        $result = $database->query('select pm10,sensor_id FROM sensors');
+        //echo "select pm10,pm25,temperature,humidity FROM sensors WHERE sensor_id = $id AND time > now() - $new_date";
+        $result = $database->query("select $properties FROM sensors WHERE sensor_id =~ /$id/ AND time > now() - $new_date");
+        //remove time from response
         $decoded = $result->getPoints();
-        //remove valies that dont neet to get send
         for ($i = 0;$i < count($decoded);$i++)
         {
-            unset($decoded[$i]['pm10']);
             unset($decoded[$i]['time']);
         }
-        // foreach($decoded as $id => $row) {
-        //     echo $row['pm10'];
-        //     unset($row['pm10']);
-        // }
         $response->getBody()->write(json_encode($decoded));
         return $response;
     });
