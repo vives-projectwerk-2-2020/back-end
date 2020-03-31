7ebb3fbc6586e2915f31929360926e8f4cdbecd6
git <arno.willems@student.vives.be>
2020-03-31 13:53:35 +0200

    mysqli_num_rows function not in this php version

--- src/routes.php
+++ src/routes.php
@@ -31,7 +31,7 @@ return function (App $app) {
             $jsontext .= $sensor_json . ",";
         }
 
-        if (mysql_num_rows($sensors) > 0) {
+        if ($sensors->rowCount() > 0) {
             $jsontext = substr_replace($jsontext, '', -1);
         }
         $jsontext .= "]";
@@ -58,7 +58,7 @@ return function (App $app) {
             $jsontext .= $user_json . ",";
         }
 
-        if (mysql_num_rows($users) > 0) {
+        if ($users->rowCount() > 0) {
             $jsontext = substr_replace($jsontext, '', -1);
         }
         $jsontext .= "]";
