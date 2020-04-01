2cc5c5266d8f7d75cd1690e10dc84dd92cf76f82
Sille Van Landschoot <info@sillevl.be>
2020-04-01 11:36:26 +0200

    add basic logger to help debugging

--- src/dependencies.php
+++ src/dependencies.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 use Psr\Container\ContainerInterface;
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
@@ -19,6 +22,18 @@ return function (ContainerBuilder $containerBuilder) {
           $eloquent->setFetchMode(PDO::FETCH_ASSOC);
 
           return $eloquent;
-      }
+      },
+      'logger' => function (ContainerInterface $c) {
+        $settings = $c->get('settings')['logger'];
+ 
+        $logger = new Logger($settings['name']);
+        $consoleHandler = new StreamHandler($settings['path'], $settings['level']);
+        // $fileHandler = new StreamHandler($settings['logfile'], $settings['level']);
+        $logger->pushHandler($fileHandler);
+        $logger->pushHandler($consoleHandler);
+ 
+        return $logger;
+      },
     ]);
+
 };
--- src/routes.php
+++ src/routes.php
@@ -15,6 +15,17 @@ use Slim\App;
 return function (App $app) {
     $app->get('/', function (Request $request, Response $response) {
 
+        $logger = $this->get('logger');
+        $logger->info("********* hello world from logger *********");
+        $logger->info("Example log levels below from HIGH to LOW");
+        $logger->emergency('Your emergency log message');
+        $logger->critical('Your critical log message');
+        $logger->error('Your error log message');
+        $logger->warning('Your warning log message');
+        $logger->notice('Your notice log message');
+        $logger->info('Your info log message');
+        $logger->debug('Your debug log message');
+
         $time = date('y-m-d\TH:i:s');
         $response->getBody()->write(json_encode($time));
         $test =  substr("qwerty", 0, 2);
