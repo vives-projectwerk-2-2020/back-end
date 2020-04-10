a3e8454735f7ef78d0316c63e73d2d978ccf53a6
Geylian lingier <geylian.l@gmail.com>
2020-04-10 11:28:48 +0200

    add more header information

--- src/middleware.php
+++ src/middleware.php
@@ -11,6 +11,8 @@ return function (App $app) {
     $app->add(function (Request $request, RequestHandler $handler) {
         $response = $handler->handle($request);
         return $response->withHeader('Content-Type', 'application/json')
-                        ->withHeader('Access-Control-Allow-Origin', '*');
+                        ->withHeader('Access-Control-Allow-Origin', '*')
+                        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
 };
