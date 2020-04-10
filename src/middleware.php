d35110e4e623c786bbed5b656d2876f55b0a6025
Geylian lingier <geylian.l@gmail.com>
2020-04-10 11:34:44 +0200

    try to solve linter error

--- src/middleware.php
+++ src/middleware.php
@@ -12,7 +12,8 @@ return function (App $app) {
         $response = $handler->handle($request);
         return $response->withHeader('Content-Type', 'application/json')
                         ->withHeader('Access-Control-Allow-Origin', '*')
-                        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, 
+                            Origin, Authorization')
                         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
 };
