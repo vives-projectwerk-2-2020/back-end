f54d93f49e119368899182fc44140919a4403a13
jarno thierens <jarno.thierens@live.be>
2020-04-29 10:44:16 +0200

    adding smaller than days

--- src/Models/Measurement.php
+++ src/Models/Measurement.php
@@ -38,18 +38,24 @@ class Measurement
 
         $new_date = $period_time . $period_range;
 
-        if ($new_date < "365d") {
+        if ($new_date <= "365d") {
 
             $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ AND time > now() - $new_date GROUP BY time(24h)";
-        } else if ($new_date < "31d") {
+        } else if ($new_date <= "31d") {
 
             $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ AND time > now() - $new_date GROUP BY time(1h)";
-        } else if ($new_date < "7d") {
+        } else if ($new_date <= "7d") {
 
             $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ AND time > now() - $new_date GROUP BY time(30m)";
-        } else if ($new_date < "1d") {
+        } else if ($new_date <= "1d") {
 
             $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ AND time > now() - $new_date GROUP BY time(5m)";
+        } else if ($new_date < "1h") {
+
+            $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ AND time > now() - $new_date GROUP BY time(5m)";
+        } else {
+
+            $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ AND time > now() - $new_date";
         }
 
         //echo "select pm10,pm25,temperature,humidity FROM sensors WHERE sensor_id = $id AND time > now() - $new_date";
@@ -57,7 +63,7 @@ class Measurement
         $result = $database->query($query);
 
         $decoded = $result->getPoints();
-        
+
         for ($i = 0; $i < count($decoded); $i++) {
             //remove time from response
             unset($decoded[$i]['time']);
