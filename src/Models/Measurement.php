541357e79a74f4dded52346259c36913a83e7be8
jarno thierens <jarno.thierens@live.be>
2020-04-29 11:47:51 +0200

    adding extra time stamps

--- src/Models/Measurement.php
+++ src/Models/Measurement.php
@@ -39,9 +39,18 @@ class Measurement
         if ($period_range == "y") {
             $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ 
                 AND time > now() - $new_date GROUP BY time(24h) FILL(0)";
-        } elseif ($period_range == "d") {
+        }elseif ($period_range == "d" && $period_time >= 31) {
+            $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ 
+                AND time > now() - $new_date GROUP BY time(1h) FILL(0)";
+        }elseif ($period_range == "d" && $period_time >= 7) {
+            $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ 
+                AND time > now() - $new_date GROUP BY time(30m) FILL(0)";
+        }elseif ($period_range == "d") {
             $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ 
                 AND time > now() - $new_date GROUP BY time(5m) FILL(0)";
+        }elseif ($period_range == "h" && $period_time > 1) {
+            $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ 
+                AND time > now() - $new_date GROUP BY time(1m) FILL(0)";
         } else {
             $query = "select $properties FROM sensors WHERE sensor_id =~ /$id/ 
                 AND time > now() - $new_date";
