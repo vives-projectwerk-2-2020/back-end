41486bf10881d70b104ba60bdff39774c93c76eb
jarno thierens <jarno.thierens@live.be>
2020-05-05 14:45:47 +0200

    24h instead of 1h

--- src/Models/Measurement.php
+++ src/Models/Measurement.php
@@ -63,7 +63,7 @@ class Measurement
         } elseif ($new_date == "1h") {
             $groupBy = "";
             $meanProperties = $properties;
-        } elseif (($new_date == "1h" || ($period == ""))) {
+        } elseif (($new_date == "24h" || ($period == ""))) {
             //Default value : 24h
             $groupBy = " GROUP BY time(5m)";
             $new_date = "24h";
