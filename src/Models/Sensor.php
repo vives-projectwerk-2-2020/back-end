c4279cb779308f4cfaeb041d477d6f9e8f4def59
git <arno.willems@student.vives.be>
2020-05-07 10:57:48 +0200

    changed primary key in sensor model

--- src/Models/Sensor.php
+++ src/Models/Sensor.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Sensor extends Model
 {
     protected $fillable = ['guid', 'name', 'latitude', 'longitude', 'city', 'address', 'description'];
+    protected $primaryKey = 'guid';
     public $timestamps = false;
     public $incrementing = false;
     protected $keyType = 'string';
