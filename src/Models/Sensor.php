890c2d64693a98ddad7fe7322fa92c5ee393001b
Michiel Decoster <decoster.michiel@gmail.com>
2020-05-07 10:09:21 +0200

    Removed id from model, replaced with guid

--- src/Models/Sensor.php
+++ src/Models/Sensor.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sensor extends Model
 {
-    protected $fillable = ['id', 'guid', 'name', 'latitude', 'longitude', 'city', 'address', 'description'];
+    protected $fillable = ['guid', 'name', 'latitude', 'longitude', 'city', 'address', 'description'];
     public $timestamps = false;
     public $incrementing = false;
     protected $keyType = 'string';
