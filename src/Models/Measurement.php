92e91bf2777c8601b5d2589c0e41eba5f31d322a
jarno thierens <jarno.thierens@live.be>
2020-05-07 15:50:01 +0200

    error concat

--- src/Models/Measurement.php
+++ src/Models/Measurement.php
@@ -86,7 +86,7 @@ class Measurement
         }
 
         if ($validProperties) {
-            $errorMessage = "ERROR: 400 Invalid properties ";
+            $errorMessage = $errorMessage."ERROR: 400 Invalid properties ";
         }
 
         $query = "select $meanProperties FROM sensors WHERE guid =~ /$id/ 
@@ -97,7 +97,7 @@ class Measurement
         $decoded = $result->getPoints();
 
         if ($errorMessage == "" &&  empty($decoded)) {
-            $errorMessage = "ERROR: 400 Invalid id ";
+            $errorMessage = $errorMessage."ERROR: 400 Invalid id ";
         }
 
 
